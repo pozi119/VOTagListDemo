2482432e5163783973d804acab39949c7f5542f4
pozi119 <Valo@YC-iMac.local>
2015-01-14 13:32:30 +0800

    nothing

---
+++ VOTagListDemo/VOTagList/VOTagList.h
@@ -0,0 +1,13 @@
+//
+//  VOTagList.h
+//  VOTagListDemo
+//
+//  Created by ValoLee on 14/12/31.
+//  Copyright (c) 2014年 ValoLee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VOTagList : UIControl
+
+@end
---
+++ VOTagListDemo/VOTagList/VOTagList.m
@@ -0,0 +1,21 @@
+//
+//  VOTagList.m
+//  VOTagListDemo
+//
+//  Created by ValoLee on 14/12/31.
+//  Copyright (c) 2014年 ValoLee. All rights reserved.
+//
+
+#import "VOTagList.h"
+
+@implementation VOTagList
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
